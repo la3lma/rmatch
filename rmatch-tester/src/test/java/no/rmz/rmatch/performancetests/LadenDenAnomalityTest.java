2965ca998bc41e77ae4d04c859178abfcb78648c
Bjørn Remseth <la3lma@gmail.com>
2023-05-16 11:48:01 +0200

    Attempt to find minimal set of failing regexps

--- rmatch-tester/src/test/java/no/rmz/rmatch/performancetests/LadenDenAnomalityTest.java
+++ rmatch-tester/src/test/java/no/rmz/rmatch/performancetests/LadenDenAnomalityTest.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2012. Bjørn Remseth (rmz@rmz.no).
  * <p>
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * <p>
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package no.rmz.rmatch.performancetests;
@@ -27,7 +27,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 
 import static no.rmz.rmatch.performancetests.BenchmarkLargeCorpus.getStringBuilderFromFileContent;
 import static no.rmz.rmatch.performancetests.BenchmarkLargeCorpus.readRegexpsFromFile;
@@ -61,14 +69,259 @@ public class LadenDenAnomalityTest {
      */
     private Matcher m;
 
+    private List<String> regexps;
+
+    private Buffer buffer;
+    private String corpus;
+
     /**
      * Instantiate test articles and set up the compiler mock
      * to simulate proper compilation of "ab" and "ac".
      */
     @BeforeEach
     public void setUp() {
-        m = new MatcherImpl();
+        this.m = new MatcherImpl();
+        final String[] corpusPaths = new String[]{"corpus/crime-and-punishment-by-dostoyevsky.txt"};
+        this.corpus = getStringBuilderFromFileContent(corpusPaths).toString();
+        this.buffer = new no.rmz.rmatch.utils.StringBuffer(corpus);
+        this.regexps = readRegexpsFromFile("corpus/unique-words.txt", 1400);
     }
+
+
+    private boolean ladenFailed(Collection<String> regexps) {
+
+        // Prepare
+        final AtomicBoolean denWasRun = new AtomicBoolean(false);
+        final AtomicBoolean ladenWasRun = new AtomicBoolean(false);
+        this.buffer = new no.rmz.rmatch.utils.StringBuffer(corpus);
+        try {
+            for (var r : regexps) {
+                switch (r) {
+                    case "den":
+                        this.m.add("den", new Action() {
+                            @Override
+                            public void performMatch(Buffer b, int start, int end) {
+                                denWasRun.set(true);
+                            }
+                        });
+                        break;
+                    case "laden":
+                        this.m.add("laden", new Action() {
+                            @Override
+                            public void performMatch(Buffer b, int start, int end) {
+                                ladenWasRun.set(true);
+                            }
+                        });
+                        break;
+                    default:
+                        this.m.add(r, defaultAction);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Act
+        m.match(buffer);
+
+        return denWasRun.get() && !ladenWasRun.get();
+    }
+
+
+    record Interval(int lower, int upper) {};
+
+    private final static class BinarySearcher {
+        private final int upperLimit;
+        private final int lowerLimit;
+        private final Predicate<Integer> upperLimitTester;
+        private final Predicate<Integer> lowerLimitTester;
+
+        BinarySearcher(int lowerLimit,
+                       int upperLimit,
+                       Predicate<Integer> upperLimitTester,
+                       Predicate<Integer> lowerLimitTester) {
+            this.upperLimit = upperLimit;
+            this.lowerLimit = lowerLimit;
+            this.upperLimitTester = upperLimitTester;
+            this.lowerLimitTester = lowerLimitTester;
+            if (lowerLimit > upperLimit) {
+                throw new RuntimeException("Upper limit larger than lower limit");
+            }
+        }
+
+        private int findLowerLimit() {
+            return findLimit(lowerLimitTester, true);
+        }
+
+        public int findUpperLimit() {
+            return findLimit(upperLimitTester, false);
+        }
+
+        public int findLimit(Predicate<Integer> tester, boolean direction) {
+            System.out.println("findLimit direction = "  + direction);
+
+            int high = upperLimit;
+            int low = lowerLimit;
+
+            // Binary search
+            while (high - low > 3) {
+                int pivot = low + ((high - low) / 2);
+                System.out.println("   Binary searching (low, high, pivot) = ("  + low + ", "+ high + ", " + pivot + ")");
+
+                if (tester.test(pivot) ^ direction) {
+                    high = pivot;
+                } else {
+                    low = pivot;
+                }
+            }
+
+
+            System.out.println("   Linear searching (low,high) = ("  + low + ", " + high + ")");
+            // Linear search
+            int result = -1;
+            if (direction) {
+                while (low < high) {
+                    result = low;
+                    System.out.println("   preliminary result ="  + result);
+
+                    if (tester.test(low + 1)) {
+                        low += 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                // from high to low
+                result = high;
+                while (low < high) {
+                    result = high;
+                    System.out.println("   preliminary result ="  + result);
+
+                    if (tester.test(high - 1)) {
+                        high -= 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            System.out.println("   findLimit result = "  + result);
+
+            return result;
+        }
+
+        public Interval findInterval() {
+            return new Interval(findLowerLimit(), findUpperLimit());
+        }
+    }
+
+    private final static class MicroLogger {
+
+        List<PrintStream> printStreams = new ArrayList<>();
+
+        public MicroLogger(File outputFile) {
+            printStreams.add(System.out);
+            try {
+                printStreams.add(new PrintStream(outputFile));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void println(String str) {
+            for (var ps : printStreams) {
+                ps.println(str);
+            }
+        }
+    }
+
+
+    /**
+     * Test matching the two regexps concurrently.
+     */
+    @Test
+    public final void findMinimalRegexpSet() throws RegexpParserException {
+
+        MicroLogger log = new MicroLogger(new File("findMinimalRegexpSetLog.txt"));
+        AtomicInteger start = new AtomicInteger(205);
+        AtomicInteger end = new AtomicInteger(this.regexps.size() - 1);
+        boolean result = true;
+
+        final BinarySearcher searcher;
+        searcher = new BinarySearcher(start.get(), end.get(),
+                new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer upperLimit) {
+                        return ladenFailed(LadenDenAnomalityTest.this.regexps.subList(start.get(), upperLimit));
+                    }
+                },
+                new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer lowerLimit) {
+                        return ladenFailed(LadenDenAnomalityTest.this.regexps.subList(lowerLimit, end.get()));
+                    }
+                });
+
+        final Interval interval = searcher.findInterval();
+/*
+        // TODO: If we need to do this more than once, then rewrite to use binary search
+        //       for both top and bottom.
+        for (; start < end && result; start += 1) {
+            result = ladenFailed(this.regexps.subList(start, end));
+            if (result) {
+                log.println("Success for start = " + start);
+            }
+        }
+
+        if (!result) {
+            start -= 1;
+        }
+
+        if (start == end) {
+            Assertions.fail("Start == end, and that is weird");
+        }
+
+        result = true;
+        for (; start < end && result; end -= 1) {
+            result = ladenFailed(this.regexps.subList(start, end));
+            if (result) {
+                log.println("Success for end = " + end);
+            }
+        }
+*/
+        start.set(interval.lower);
+        end.set(interval.upper);
+
+        // Just testing that this will indeed fail laden
+        var sublist = new ArrayList<String>();
+        sublist.addAll(regexps.subList(start.get(), end.get()));
+        if (ladenFailed(sublist)) {
+            System.out.println("Yes. this interval works!: " + start.get() + "," + end.get());
+            for (int i = start.get() ; i <= end.get(); i++) {
+                System.out.println(" i = " + i + ", regex = " + regexps.get(i));
+            }
+        } else {
+            System.out.println("No. this interval does not works!: " + start.get() + "," + end.get());
+        }
+
+
+        System.out.println("Determined boundaries: start = " + start + ", end = " + end);
+        System.out.println("Trying to remove individual regexps");
+
+        for (int i = start.get(); i < end.get(); i += 1) {
+            var testlist = new ArrayList<String>();
+            testlist.addAll(regexps.subList(start.get(), end.get()));
+            testlist.remove(i - start.get());
+            result = ladenFailed(testlist);
+            if (result) {
+                log.println("Success for removal of i = " + i + ", regex = " + this.regexps.get(i));
+            } else {
+                log.println("Failure for removal of i = " + i + ",  regex = " + this.regexps.get(i));
+            }
+        }
+        System.out.println("Done.");
+    }
+
 
     /**
      * Test matching the two regexps concurrently.
@@ -77,26 +330,24 @@ public class LadenDenAnomalityTest {
     public final void testUseOfOrdinaryMatcherOn1500SubsetOfRegexps() throws RegexpParserException {
 
         // Prepare
-        final String[] corpusPaths = new String[]{"corpus/crime-and-punishment-by-dostoyevsky.txt"};
-        final String corpus = getStringBuilderFromFileContent(corpusPaths).toString();
-        final no.rmz.rmatch.utils.StringBuffer b =
-                new no.rmz.rmatch.utils
-                .StringBuffer(corpus);
-        final List<String> regexps = readRegexpsFromFile("corpus/unique-words.txt", 1400);
-
-        for (var r : regexps) {
+        for (var r : this.regexps) {
             switch (r) {
-                case "den":  m.add("den", denAction); break;
-                case "laden":  m.add("laden", ladenAction); break;
-                default:  m.add(r, defaultAction);
+                case "den":
+                    m.add("den", denAction);
+                    break;
+                case "laden":
+                    m.add("laden", ladenAction);
+                    break;
+                default:
+                    m.add(r, defaultAction);
             }
         }
 
         // Act
-        m.match(b);
+        m.match(buffer);
 
         // Verify
         verify(ladenAction).performMatch(any(Buffer.class), eq(128310), eq(128314));
-        verify(denAction).performMatch(any(Buffer.class),   eq(128312), eq(128314));
+        verify(denAction).performMatch(any(Buffer.class), eq(128312), eq(128314));
     }
 }
