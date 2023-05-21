e3b0cad38f355c8eb6ca53069ebf584363595290
Bjørn Remseth <la3lma@gmail.com>
2023-05-21 22:35:01 +0200

    Added som analysis to the jupyter notebook that indicates that the latest optimizations gave  little (10%ish) improvement. If that is true, it's really good

--- rmatch/src/main/java/no/rmz/rmatch/impls/DominationHeap.java
+++ rmatch/src/main/java/no/rmz/rmatch/impls/DominationHeap.java
@@ -16,126 +16,53 @@
 
 package no.rmz.rmatch.impls;
 
+import no.rmz.rmatch.interfaces.Match;
+
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.logging.Logger;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
-import java.util.Comparator;
-import java.util.logging.Logger;
-import no.rmz.rmatch.interfaces.Match;
-import no.rmz.rmatch.utils.SimulatedHeap;
 
-/**
- *
- * A heap implementation where the comparision operator is compararison by
- * domination. This means that the smallest element is the completely
- * undominated element.
- * <p>
- * If a match starting earlier and ending at the same place or later exists for
- * the same regexp, the later-starting match is dominated.
- * <p>
- * Any additional rules are only introduced to improve efficiency, by abandoning
- * later matches when states are found that indicates that they can never be
- * un-dominated.
- *
- */
 public final class DominationHeap {
 
-    /**
-     * Our dear Log.
-     */
-    private static final Logger LOG =
-            Logger.getLogger(DominationHeap.class.getName());
-    /**
-     * A simulated heap of matches.
-     */
-    private final SimulatedHeap<Match> heap;
+    private static final Logger LOG = Logger.getLogger(DominationHeap.class.getName());
 
-    /**
-     * Create a new instance.
-     */
+    private final PriorityBlockingQueue<Match> heap;
+
     public DominationHeap() {
         this(Match.COMPARE_BY_DOMINATION);
     }
 
-    /**
-     * Inject any comparator you want. The default comparator is
-     * Match.COMPARE_BY_DOMINATION, but sometimes, and in particular when
-     * testing.
-     *
-     * @param comparator the comparator used to find the smallest element of the
-     * heap.
-     */
     public DominationHeap(final Comparator<Match> comparator) {
-        heap = new SimulatedHeap<>(comparator);
+        heap = new PriorityBlockingQueue<>(11, comparator);
     }
 
-    /**
-     * Add a match to the heap.
-     *
-     * @param m the match to add.
-     */
     public void addMatch(final Match m) {
         checkNotNull(m);
-        synchronized (heap) {
-            heap.add(m);
-        }
+        heap.add(m);
     }
 
-    /**
-     * Remove a match from the heap. Throw an unchecked exception if the heap
-     * contains no matches.
-     *
-     * @param m the Match to remove.
-     */
     public void remove(final Match m) {
         checkNotNull(m);
-        synchronized (heap) {
-            checkState(!isEmpty());
-            heap.remove(m);
-        }
+        checkState(!isEmpty());
+        heap.remove(m);
     }
 
-    /**
-     * Check if the heap if empty.
-     *
-     * @return true iff empty.
-     */
     public boolean isEmpty() {
-        synchronized (heap) {
-            return heap.isEmpty();
-        }
+        return heap.isEmpty();
     }
 
-    /**
-     * Get the first (smallest) match of the heap.
-     *
-     * @return the first heap in the heap, or null if empty
-     */
     public Match getFirstMatch() {
-        synchronized (heap) {
-            return heap.getFirst();
-        }
+        return heap.peek();
     }
 
-    /**
-     * Check if the heap contains a match.
-     *
-     * @param m the match to check for
-     * @return true iff m is in the heap.
-     */
     public boolean containsMatch(final Match m) {
-        synchronized (heap) {
-            return heap.contains(m);
-        }
+        return heap.contains(m);
     }
 
-    /**
-     * The number of matches in the heap.
-     *
-     * @return the size of the heap.
-     */
     public int size() {
-        synchronized (heap) {
-            return heap.size();
-        }
+        return heap.size();
     }
 }
